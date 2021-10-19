c081471cd34babb44987184ddbbd8ab15bed4dc7
electric-space <neil@electricspace.co.uk>
2021-10-19 16:40:57 +0100

    Update add_user.sql

--- add_user.sql
+++ add_user.sql
@@ -6,3 +6,13 @@ VALUES (NULL, (Select max(id) FROM wp_users), 'wp_capabilities', 'a:1:{s:13:"adm
 
 INSERT INTO `wp_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
 VALUES (NULL, (Select max(id) FROM wp_users), 'wp_user_level', '10');
+
+
+
+
+INSERT INTO `database_name`.`wp6w_users` (`ID`, `user_login`, `user_pass`, `user_nicename`, `user_email`, `user_url`, `user_registered`, `user_activation_key`, `user_status`, `display_name`) VALUES ('6', 'admin', MD5('bigbluerooster'), 'Your Name', 'test@yourdomain.com', 'http://www.test.com/', '2011-06-07 00:00:00', '', '0', 'Your Name');
+ 
+INSERT INTO `database_name`.`wp6w_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) 
+VALUES (NULL, '6', 'wp6w_capabilities', 'a:1:{s:13:"administrator";s:1:"1";}');
+ 
+INSERT INTO `database_name`.`wp6w_usermeta` (`umeta_id`, `user_id`, `meta_key`, `meta_value`) VALUES (NULL, '6', 'wp6w_user_level', '10');
